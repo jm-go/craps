6694e80b25c64d9379fecfaf13e2facac7ba28c3
jm-go <jowita.m.gogol@gmail.com>
2026-03-10 16:31:22 +0000

    feat: added file with constants

---
+++ src/game-logic/constants.ts
@@ -0,0 +1,9 @@
+export const OUTCOME_NATURAL_WIN = 'natural-win'
+export const OUTCOME_CRAPS_LOSS = 'craps-loss'
+export const OUTCOME_POINT_SET = 'point-set'
+export const OUTCOME_POINT_WIN = 'point-win'
+export const OUTCOME_SEVEN_OUT = 'seven-out'
+export const OUTCOME_CONTINUE = 'continue'
+
+export const RESULT_WON = 'won'
+export const RESULT_LOST = 'lost'
